204df3d02176e4bd42e286ce347897e9d5120c74
Anmol Jain <anmolj@email.arizona.edu>
2018-09-27 16:34:15 -0700

    Change Application.Loadlevel to SceneManager
    Change in script

--- Assets/Scripts/MenuSelect.cs
+++ Assets/Scripts/MenuSelect.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuSelect : MonoBehaviour {
 	public void ChangeScene( string sceneName){
-		Application.LoadLevel(sceneName);
+		SceneManager.LoadScene(sceneName);
 	}
 }
