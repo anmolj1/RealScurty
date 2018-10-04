b8daa3040f6b37e99b5cc578338b8e8c4fdfeef4
Anmol Jain <anmolj@email.arizona.edu>
2018-10-04 13:42:10 -0700

    Added scripts

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+
+	void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (collision2D.transform.name == "Player"){
+        
+        var hit = collision2D.gameObject;
+        var Health = hit.GetComponent<Health>();
+        
+        if (Health  != null)
+        {
+            Health.TakeDamage(10);
+            print(PlayerController.singleton.myAnimator.GetCurrentAnimatorStateInfo(0));
+
+         }
+    
+
+        if  (PlayerController.singleton.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("idle")){
+
+            Destroy(gameObject);
+        }
+            
+        }
+
+    }
+}
+
